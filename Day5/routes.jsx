d55985f82d77df8e1d69d11374f6297c12a206c0
Abhilash S Naik <129758286+abhi-chethu@users.noreply.github.com>
2023-10-28 22:55:45 +0530

    Create routes.jsx

---
+++ Day5/routes.jsx
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+const route=require('./routes/routes')
+
+
+app.use(express.json());
+
+const PORT = 8000;
+
+mongoose.connect('mongodb+srv://abhishek:abhishek@cluster0.6bksxua.mongodb.net/')
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.use('/', route);
+
+app.listen(PORT, () => {
+  console.log(Server is running on port ${PORT});
+});
