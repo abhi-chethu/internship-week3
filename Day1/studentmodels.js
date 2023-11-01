8935e1cdf17c8fc2b996c9bc96cf74073ff57dae
Abhilash S Naik <129758286+abhi-chethu@users.noreply.github.com>
2023-11-01 10:36:25 +0530

    Create studentmodels.js

---
+++ Day1/studentmodels.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const studentSchema = new mongoose.Schema({
+    fname : {
+        type : String,
+    },
+    lname : {
+        type : String,
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    password : {
+        type : String,
+        required : true,
+    },
+    mobile:{
+        type : String,
+        required:true,
+    },
+    address : {
+        type : String,
+    }
+    },  {timestamps : true
+
+})
+
+module.exports = mongoose.model('Student',studentSchema)
